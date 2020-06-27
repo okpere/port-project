1ad1d81a497b9f04f46b03559021626ca0da1164
clinton okpere <clintonokpere99@gmail.com>
2020-06-27 23:37:09 +0100

    just added job and blog function

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200, default='DEFAULT VALUE')
+    pub_date = models.DateField(auto_now=False, auto_now_add=False,)
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200, default='DEFAULT VALUE')
